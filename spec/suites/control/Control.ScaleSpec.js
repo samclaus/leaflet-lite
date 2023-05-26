0db211eeeaf10ad8edbaf4d4690d280b77aaecbd
Sam Claus <samclaus@protonmail.com>
2023-05-26 18:17:27 -0400

    Remove a lot of stuff

--- spec/suites/control/Control.ScaleSpec.js
+++
@@ -1,6 +0,0 @@
-describe('Control.Scale', () => {
-	it('can be added to an unloaded map', () => {
-		const map = L.map(document.createElement('div'));
-		new L.Control.Scale().addTo(map);
-	});
-});
--- spec/suites/core/GeneralSpec.js
+++
@@ -1,9 +0,0 @@
-describe('General', () => {
-	it('namespace extension', () => {
-		L.Util.foo = 'bar';
-		L.Foo = 'Bar';
-
-		expect(L.Util.foo).to.eql('bar');
-		expect(L.Foo).to.eql('Bar');
-	});
-});
