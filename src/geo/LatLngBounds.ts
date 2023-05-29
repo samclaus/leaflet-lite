d91438109d747d54c5a45f36932becc9398f0cef
Sam Claus <samclaus@protonmail.com>
2023-05-29 01:30:54 -0400

    Progress typing/simplifying points and bounds

--- src/geo/LatLngBounds.ts
+++ src/geo/LatLngBounds.ts
@@ -28,19 +28,18 @@ import {LatLng, toLatLng} from './LatLng.js';
  * Note that `LatLngBounds` does not inherit from Leaflet's `Class` object,
  * which means new classes can't inherit from it, and new methods
  * can't be added to it with the `include` function.
+ * 
+ * TODO International date line?
  */
+export class LatLngBounds {
 
-export function LatLngBounds(corner1, corner2) { // (LatLng, LatLng) or (LatLng[])
-	if (!corner1) { return; }
-
-	const latlngs = corner2 ? [corner1, corner2] : corner1;
-
-	for (let i = 0, len = latlngs.length; i < len; i++) {
-		this.extend(latlngs[i]);
+	constructor(corner1, corner2) { // (LatLng, LatLng) or (LatLng[])
+		const latlngs = corner2 ? [corner1, corner2] : corner1;
+	
+		for (let i = 0, len = latlngs.length; i < len; i++) {
+			this.extend(latlngs[i]);
+		}
 	}
-}
-
-LatLngBounds.prototype = {
 
 	// @method extend(latlng: LatLng): this
 	// Extend the bounds to contain the given point
@@ -78,7 +77,7 @@ LatLngBounds.prototype = {
 		}
 
 		return this;
-	},
+	}
 
 	// @method pad(bufferRatio: Number): LatLngBounds
 	// Returns bounds created by extending or retracting the current bounds by a given ratio in each direction.
@@ -93,7 +92,7 @@ LatLngBounds.prototype = {
 		return new LatLngBounds(
 		        new LatLng(sw.lat - heightBuffer, sw.lng - widthBuffer),
 		        new LatLng(ne.lat + heightBuffer, ne.lng + widthBuffer));
-	},
+	}
 
 	// @method getCenter(): LatLng
 	// Returns the center point of the bounds.
@@ -101,55 +100,55 @@ LatLngBounds.prototype = {
 		return new LatLng(
 		        (this._southWest.lat + this._northEast.lat) / 2,
 		        (this._southWest.lng + this._northEast.lng) / 2);
-	},
+	}
 
 	// @method getSouthWest(): LatLng
 	// Returns the south-west point of the bounds.
 	getSouthWest() {
 		return this._southWest;
-	},
+	}
 
 	// @method getNorthEast(): LatLng
 	// Returns the north-east point of the bounds.
 	getNorthEast() {
 		return this._northEast;
-	},
+	}
 
 	// @method getNorthWest(): LatLng
 	// Returns the north-west point of the bounds.
 	getNorthWest() {
 		return new LatLng(this.getNorth(), this.getWest());
-	},
+	}
 
 	// @method getSouthEast(): LatLng
 	// Returns the south-east point of the bounds.
 	getSouthEast() {
 		return new LatLng(this.getSouth(), this.getEast());
-	},
+	}
 
 	// @method getWest(): Number
 	// Returns the west longitude of the bounds
 	getWest() {
 		return this._southWest.lng;
-	},
+	}
 
 	// @method getSouth(): Number
 	// Returns the south latitude of the bounds
 	getSouth() {
 		return this._southWest.lat;
-	},
+	}
 
 	// @method getEast(): Number
 	// Returns the east longitude of the bounds
 	getEast() {
 		return this._northEast.lng;
-	},
+	}
 
 	// @method getNorth(): Number
 	// Returns the north latitude of the bounds
 	getNorth() {
 		return this._northEast.lat;
-	},
+	}
 
 	// @method contains(otherBounds: LatLngBounds): Boolean
 	// Returns `true` if the rectangle contains the given one.
@@ -177,7 +176,7 @@ LatLngBounds.prototype = {
 
 		return (sw2.lat >= sw.lat) && (ne2.lat <= ne.lat) &&
 		       (sw2.lng >= sw.lng) && (ne2.lng <= ne.lng);
-	},
+	}
 
 	// @method intersects(otherBounds: LatLngBounds): Boolean
 	// Returns `true` if the rectangle intersects the given bounds. Two bounds intersect if they have at least one point in common.
@@ -193,7 +192,7 @@ LatLngBounds.prototype = {
 		    lngIntersects = (ne2.lng >= sw.lng) && (sw2.lng <= ne.lng);
 
 		return latIntersects && lngIntersects;
-	},
+	}
 
 	// @method overlaps(otherBounds: LatLngBounds): Boolean
 	// Returns `true` if the rectangle overlaps the given bounds. Two bounds overlap if their intersection is an area.
@@ -209,13 +208,13 @@ LatLngBounds.prototype = {
 		    lngOverlaps = (ne2.lng > sw.lng) && (sw2.lng < ne.lng);
 
 		return latOverlaps && lngOverlaps;
-	},
+	}
 
 	// @method toBBoxString(): String
 	// Returns a string with bounding box coordinates in a 'southwest_lng,southwest_lat,northeast_lng,northeast_lat' format. Useful for sending requests to web services that return geo data.
 	toBBoxString() {
 		return [this.getWest(), this.getSouth(), this.getEast(), this.getNorth()].join(',');
-	},
+	}
 
 	// @method equals(otherBounds: LatLngBounds, maxMargin?: Number): Boolean
 	// Returns `true` if the rectangle is equivalent (within a small margin of error) to the given bounds. The margin of error can be overridden by setting `maxMargin` to a small number.
@@ -226,26 +225,13 @@ LatLngBounds.prototype = {
 
 		return this._southWest.equals(bounds.getSouthWest(), maxMargin) &&
 		       this._northEast.equals(bounds.getNorthEast(), maxMargin);
-	},
+	}
 
 	// @method isValid(): Boolean
 	// Returns `true` if the bounds are properly initialized.
+	/** @deprecated Bounds should never be constructed with an "invalid" state-makes no sense */
 	isValid() {
 		return !!(this._southWest && this._northEast);
 	}
-};
 
-// TODO International date line?
-
-// @factory L.latLngBounds(corner1: LatLng, corner2: LatLng)
-// Creates a `LatLngBounds` object by defining two diagonally opposite corners of the rectangle.
-
-// @alternative
-// @factory L.latLngBounds(latlngs: LatLng[])
-// Creates a `LatLngBounds` object defined by the geographical points it contains. Very useful for zooming the map to fit a particular set of locations with [`fitBounds`](#map-fitbounds).
-export function toLatLngBounds(a, b) {
-	if (a instanceof LatLngBounds) {
-		return a;
-	}
-	return new LatLngBounds(a, b);
 }
--- src/geometry/Bounds.ts
+++ src/geometry/Bounds.ts
@@ -1,4 +1,4 @@
-import {Point, toPoint} from './Point.js';
+import { Point } from './Point.js';
 
 /*
  * @class Bounds
@@ -24,18 +24,21 @@ import {Point, toPoint} from './Point.js';
  * which means new classes can't inherit from it, and new methods
  * can't be added to it with the `include` function.
  */
+export class Bounds {
 
-export function Bounds(a, b) {
-	if (!a) { return; }
+	min: Point;
+	max: Point;
 
-	const points = b ? [a, b] : a;
-
-	for (let i = 0, len = points.length; i < len; i++) {
-		this.extend(points[i]);
+	constructor(a, b) {
+		if (!a) { return; }
+	
+		const points = b ? [a, b] : a;
+	
+		for (let i = 0; i < points.length; i++) {
+			this.extend(points[i]);
+		}
 	}
-}
-
-Bounds.prototype = {
+	
 	// @method extend(point: Point): this
 	// Extends the bounds to contain the given point.
 
@@ -70,59 +73,57 @@ Bounds.prototype = {
 			this.max.y = Math.max(max2.y, this.max.y);
 		}
 		return this;
-	},
+	}
 
 	// @method getCenter(round?: Boolean): Point
 	// Returns the center point of the bounds.
-	getCenter(round) {
-		return toPoint(
-		        (this.min.x + this.max.x) / 2,
-		        (this.min.y + this.max.y) / 2, round);
-	},
+	getCenter(round?: boolean): Point {
+		return new Point(
+			(this.min.x + this.max.x) / 2,
+			(this.min.y + this.max.y) / 2,
+			round,
+		);
+	}
 
 	// @method getBottomLeft(): Point
 	// Returns the bottom-left point of the bounds.
-	getBottomLeft() {
-		return toPoint(this.min.x, this.max.y);
-	},
+	getBottomLeft(): Point {
+		return new Point(this.min.x, this.max.y);
+	}
 
 	// @method getTopRight(): Point
 	// Returns the top-right point of the bounds.
-	getTopRight() { // -> Point
-		return toPoint(this.max.x, this.min.y);
-	},
+	getTopRight(): Point {
+		return new Point(this.max.x, this.min.y);
+	}
 
 	// @method getTopLeft(): Point
 	// Returns the top-left point of the bounds (i.e. [`this.min`](#bounds-min)).
-	getTopLeft() {
-		return this.min; // left, top
-	},
+	getTopLeft(): Point {
+		return this.min;
+	}
 
 	// @method getBottomRight(): Point
 	// Returns the bottom-right point of the bounds (i.e. [`this.max`](#bounds-max)).
-	getBottomRight() {
-		return this.max; // right, bottom
-	},
+	getBottomRight(): Point {
+		return this.max;
+	}
 
 	// @method getSize(): Point
 	// Returns the size of the given bounds
-	getSize() {
+	getSize(): Point {
 		return this.max.subtract(this.min);
-	},
+	}
 
 	// @method contains(otherBounds: Bounds): Boolean
 	// Returns `true` if the rectangle contains the given one.
 	// @alternative
 	// @method contains(point: Point): Boolean
 	// Returns `true` if the rectangle contains the given point.
-	contains(obj) {
-		let min, max;
-
-		if (typeof obj[0] === 'number' || obj instanceof Point) {
-			obj = toPoint(obj);
-		} else {
-			obj = toBounds(obj);
-		}
+	// TODO: make this not polymorphic, add "fromPoint" constructor for Bounds class
+	contains(obj: Point | Bounds): boolean {
+		let min: Point;
+		let max: Point;
 
 		if (obj instanceof Bounds) {
 			min = obj.min;
@@ -131,19 +132,20 @@ Bounds.prototype = {
 			min = max = obj;
 		}
 
-		return (min.x >= this.min.x) &&
-		       (max.x <= this.max.x) &&
-		       (min.y >= this.min.y) &&
-		       (max.y <= this.max.y);
-	},
+		return (
+			(min.x >= this.min.x) &&
+			(max.x <= this.max.x) &&
+			(min.y >= this.min.y) &&
+			(max.y <= this.max.y)
+		);
+	}
 
 	// @method intersects(otherBounds: Bounds): Boolean
 	// Returns `true` if the rectangle intersects the given bounds. Two bounds
 	// intersect if they have at least one point in common.
-	intersects(bounds) { // (Bounds) -> Boolean
-		bounds = toBounds(bounds);
-
-		const min = this.min,
+	intersects(bounds: Bounds): boolean {
+		const
+			min = this.min,
 		    max = this.max,
 		    min2 = bounds.min,
 		    max2 = bounds.max,
@@ -151,15 +153,14 @@ Bounds.prototype = {
 		    yIntersects = (max2.y >= min.y) && (min2.y <= max.y);
 
 		return xIntersects && yIntersects;
-	},
+	}
 
 	// @method overlaps(otherBounds: Bounds): Boolean
 	// Returns `true` if the rectangle overlaps the given bounds. Two bounds
 	// overlap if their intersection is an area.
-	overlaps(bounds) { // (Bounds) -> Boolean
-		bounds = toBounds(bounds);
-
-		const min = this.min,
+	overlaps(bounds: Bounds): boolean {
+		const
+			min = this.min,
 		    max = this.max,
 		    min2 = bounds.min,
 		    max2 = bounds.max,
@@ -167,53 +168,39 @@ Bounds.prototype = {
 		    yOverlaps = (max2.y > min.y) && (min2.y < max.y);
 
 		return xOverlaps && yOverlaps;
-	},
+	}
 
 	// @method isValid(): Boolean
 	// Returns `true` if the bounds are properly initialized.
+	/** @deprecated Bounds should never be constructed with an "invalid" state-makes no sense */
 	isValid() {
 		return !!(this.min && this.max);
-	},
-
+	}
 
 	// @method pad(bufferRatio: Number): Bounds
 	// Returns bounds created by extending or retracting the current bounds by a given ratio in each direction.
 	// For example, a ratio of 0.5 extends the bounds by 50% in each direction.
 	// Negative values will retract the bounds.
-	pad(bufferRatio) {
-		const min = this.min,
-		max = this.max,
-		heightBuffer = Math.abs(min.x - max.x) * bufferRatio,
-		widthBuffer = Math.abs(min.y - max.y) * bufferRatio;
+	pad(bufferRatio: number): Bounds {
+		const
+			min = this.min,
+			max = this.max,
+			heightBuffer = Math.abs(min.x - max.x) * bufferRatio,
+			widthBuffer = Math.abs(min.y - max.y) * bufferRatio;
 
-
-		return toBounds(
-			toPoint(min.x - heightBuffer, min.y - widthBuffer),
-			toPoint(max.x + heightBuffer, max.y + widthBuffer));
-	},
-
+		return new Bounds(
+			new Point(min.x - heightBuffer, min.y - widthBuffer),
+			new Point(max.x + heightBuffer, max.y + widthBuffer),
+		);
+	}
 
 	// @method equals(otherBounds: Bounds): Boolean
 	// Returns `true` if the rectangle is equivalent to the given bounds.
-	equals(bounds) {
-		if (!bounds) { return false; }
-
-		bounds = toBounds(bounds);
-
-		return this.min.equals(bounds.getTopLeft()) &&
-			this.max.equals(bounds.getBottomRight());
-	},
-};
-
-
-// @factory L.bounds(corner1: Point, corner2: Point)
-// Creates a Bounds object from two corners coordinate pairs.
-// @alternative
-// @factory L.bounds(points: Point[])
-// Creates a Bounds object from the given array of points.
-export function toBounds(a, b) {
-	if (!a || a instanceof Bounds) {
-		return a;
+	equals(bounds: Bounds): boolean {
+		return (
+			this.min.equals(bounds.min) &&
+			this.max.equals(bounds.max)
+		);
 	}
-	return new Bounds(a, b);
+
 }
