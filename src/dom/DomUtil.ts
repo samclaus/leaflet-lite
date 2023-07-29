211981add17a51f6f311a6881d7f50cb4c564e05
Sam Claus <samclaus@protonmail.com>
2023-07-28 20:25:39 -0400

    Clean up DomUtil code

--- src/dom/DomUtil.ts
+++ src/dom/DomUtil.ts
@@ -1,24 +1,8 @@
 import { Point } from '../geom';
 import * as DomEvent from './DomEvent.js';
 
+// TODO: this is just a temporary fix to break a circular dependency
 export { getScale } from './get-scale.js';
-
-/*
- * @namespace DomUtil
- *
- * Utility functions to work with the [DOM](https://developer.mozilla.org/docs/Web/API/Document_Object_Model)
- * tree, used by Leaflet internally.
- *
- * Most functions expecting or returning a `HTMLElement` also work for
- * SVG elements. The only difference is that classes refer to CSS classes
- * in HTML and SVG classes in SVG.
- */
-
-// Returns an element given its DOM id, or returns the element itself
-// if it was passed directly.
-export function get(id: string | HTMLElement): HTMLElement | null {
-	return typeof id === 'string' ? document.getElementById(id) : id;
-}
 
 /**
  * Creates an HTML element with `tagName`, sets its class to `className`,
@@ -53,7 +37,9 @@ export function removeAllChildren(node: Node): void {
 	}
 }
 
-// Makes `el` the last child of its parent, so it renders in front of the other children.
+/**
+ * Makes `el` the last child of its parent, so it renders in front of the other children.
+ */
 export function toFront(el: HTMLElement): void {
 	const parent = el.parentNode;
 
@@ -62,7 +48,9 @@ export function toFront(el: HTMLElement): void {
 	}
 }
 
-// Makes `el` the first child of its parent, so it renders behind the other children.
+/**
+ * Makes `el` the first child of its parent, so it renders behind the other children.
+ */
 export function toBack(el: HTMLElement): void {
 	const parent = el.parentNode;
 
@@ -71,9 +59,11 @@ export function toBack(el: HTMLElement): void {
 	}
 }
 
-// Resets the 3D CSS transform of `el` so it is translated by `offset` pixels
-// and optionally scaled by `scale`. Does not have an effect if the
-// browser doesn't support 3D CSS transforms.
+/**
+ * Resets the 3D CSS transform of `el` so it is translated by `offset` pixels
+ * and optionally scaled by `scale`. Does not have an effect if the
+ * browser doesn't support 3D CSS transforms.
+ */
 export function setTransform(el: HTMLElement, offset?: Point, scale?: number): void {
 	const {x, y} = offset || new Point(0, 0);
 
@@ -82,15 +72,19 @@ export function setTransform(el: HTMLElement, offset?: Point, scale?: number): v
 
 const positions = new WeakMap<HTMLElement, Point>();
 
-// Sets the position of `el` to coordinates specified by `position`,
-// using CSS translate or top/left positioning depending on the browser
-// (used by Leaflet internally to position its layers).
+/**
+ * Sets the position of `el` to coordinates specified by `position`,
+ * using CSS translate or top/left positioning depending on the browser
+ * (used by Leaflet internally to position its layers).
+ */
 export function setPosition(el: HTMLElement, point: Point): void {
 	positions.set(el, point);
 	setTransform(el, point);
 }
 
-// Returns the coordinates of an element previously positioned with setPosition.
+/**
+ * Returns the coordinates of an element previously positioned with setPosition.
+ */
 export function getPosition(el: HTMLElement): Point {
 	// this method is only used for elements previously positioned using setPosition,
 	// so it's safe to cache the position for performance
@@ -103,9 +97,11 @@ const userSelectProp = ['userSelect', 'WebkitUserSelect'].find(prop => prop in d
 
 let prevUserSelect: any;
 
-// Prevents the user from selecting text in the document. Used internally
-// by Leaflet to override the behaviour of any click-and-drag interaction on
-// the map. Affects drag interactions on the whole document.
+/**
+ * Prevents the user from selecting text in the document. Used internally
+ * by Leaflet to override the behaviour of any click-and-drag interaction on
+ * the map. Affects drag interactions on the whole document.
+ */
 export function disableTextSelection(): void {
 	const value = documentStyle[userSelectProp];
 
@@ -117,7 +113,9 @@ export function disableTextSelection(): void {
 	documentStyle[userSelectProp] = 'none';
 }
 
-// Cancels the effects of a previous [`L.DomUtil.disableTextSelection`](#domutil-disabletextselection).
+/**
+ * Cancels the effects of a previous `disableTextSelection()` call.
+ */
 export function enableTextSelection(): void {
 	if (typeof prevUserSelect === 'undefined') {
 		return;
@@ -127,26 +125,35 @@ export function enableTextSelection(): void {
 	prevUserSelect = undefined;
 }
 
-// Prevents the user from generating `dragstart` DOM events, usually generated when the user drags an image.
+/**
+ * Prevents the user from generating `dragstart` DOM events, usually generated
+ * when the user drags an image.
+ */
 export function disableImageDrag(): void {
-	// TODO: refactor/remove DOM event code and make this less janky
-	DomEvent.on(window as unknown as HTMLElement, 'dragstart', DomEvent.preventDefault);
+	DomEvent.on(window, 'dragstart', DomEvent.preventDefault);
 }
 
-// Cancels the effects of a previous [`L.DomUtil.disableImageDrag`](#domutil-disableimagedrag).
+/**
+ * Cancels the effects of a previous `disableImageDrag()` call.
+ */
 export function enableImageDrag(): void {
-	// TODO: refactor/remove DOM event code and make this less janky
-	DomEvent.off(window as unknown as HTMLElement, 'dragstart', DomEvent.preventDefault);
+	DomEvent.off(window, 'dragstart', DomEvent.preventDefault);
 }
 
 let
 	_outlineElement: HTMLElement | undefined,
 	_outlineStyle: string | undefined;
 
-// Makes the [outline](https://developer.mozilla.org/docs/Web/CSS/outline)
-// of the element `el` invisible. Used internally by Leaflet to prevent
-// focusable elements from displaying an outline when the user performs a
-// drag interaction on them.
+/**
+ * Makes the [outline](https://developer.mozilla.org/docs/Web/CSS/outline)
+ * of the element `el` invisible. Used internally by Leaflet to prevent
+ * focusable elements from displaying an outline when the user performs a
+ * drag interaction on them.
+ * 
+ * NOTE: calling this function will restore the outline style of the previous
+ * element it was called for, if there is one. The function may only affect
+ * one element at a time.
+ */
 export function preventOutline(element: HTMLElement): void {
 	while (element.tabIndex === -1 && element.parentNode) {
 		element = element.parentNode as HTMLElement;
@@ -156,23 +163,25 @@ export function preventOutline(element: HTMLElement): void {
 	_outlineElement = element;
 	_outlineStyle = element.style.outlineStyle;
 	element.style.outlineStyle = 'none';
-	// TODO: refactor/remove DOM event code and make this less janky
-	DomEvent.on(window as unknown as HTMLElement, 'keydown', restoreOutline);
+	DomEvent.on(window, 'keydown', restoreOutline);
 }
 
 /**
- * Cancels the effects of a previous [`L.DomUtil.preventOutline`](#domutil-preventoutline).
+ * Cancels the effects of a previous `preventOutline()` call.
  */
 export function restoreOutline(): void {
-	if (!_outlineElement) { return; }
-	_outlineElement.style.outlineStyle = _outlineStyle!;
-	_outlineElement = undefined;
-	_outlineStyle = undefined;
-	// TODO: refactor/remove DOM event code and make this less janky
-	DomEvent.off(window as unknown as HTMLElement, 'keydown', restoreOutline);
+	if (_outlineElement) {
+		_outlineElement.style.outlineStyle = _outlineStyle!;
+		_outlineElement = undefined;
+		_outlineStyle = undefined;
+
+		DomEvent.off(window, 'keydown', restoreOutline);
+	}
 }
 
-// Finds the closest parent node which size (width and height) is not null.
+/**
+ * Finds the closest parent node for which size (width and height) is not null or 0.
+ */
 export function getSizedParentNode(element: HTMLElement): HTMLElement | undefined {
 	do {
 		// IMPORTANT: parentNode might be 'null' but I don't want to create a
