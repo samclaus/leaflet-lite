9e44bc77609e48a13abe073bed05075be9af3de1
Sam Claus <samclaus@protonmail.com>
2023-10-18 14:51:48 -0400

    Remove LayerGroup, plan GeoJSON changes

--- src/map-elem/GeoJSON.ts
+++ src/map-elem/GeoJSON.ts
@@ -6,6 +6,235 @@
 // import { LayerGroup } from './LayerGroup.js';
 // import { Marker } from './marker';
 // import { CircleMarker, Polygon, Polyline } from './vector';
+// import { Util } from '../core';
+// import { LatLngBounds } from '../geog';
+// import { Layer } from '.';
+// import type { PathOptions } from './vector';
+// import type { Map } from '../map';
+
+/**
+ * READ THIS COMMENT BEFORE TOUCHING THIS FILE
+ * 
+ * Previously, Leaflet had a ton of functionality to support GeoJSON. First of all,
+ * it had a classes FeatureGroup and LayerGroup (I can't remember which subclassed
+ * which) that extended the main Layer class to walk and talk like a single object on
+ * the map while actually holding references to many objects/elements (markers, video
+ * overlays, polylines, etc.) on the map. The GeoJSON class then subclassed that
+ * functionality to group all of the polylines, polygons, circles, markers, etc. that
+ * were parsed from GeoJSON data. This let you easily, say, add a tooltip to the entire
+ * cluster of things on the map.
+ * 
+ * However, all of this religious subclassing and obsession with uniformity came at the
+ * cost of complexity (both mentally for the programmer and performance-wise for the
+ * computer) and code bloat. Vector objects (such as polylines and circles) are no
+ * longer full "map element" citizens because they do not even correspond to a DOM
+ * element if they are rendered with a Canvas. Additionally, LayerGroup didn't actually
+ * seem all that useful in practice. I am preferring to have individual map elements be
+ * very lightweight without full-fledged event handling, etc., and the user of the library
+ * can simply listener for 'mouseover' events on the map itself and rely on event bubbling
+ * and an API for easily figuring out which element the event came from to decide what to
+ * do. This is generally referred to as "event delegation" on the web, and is much better
+ * for performance (and the programmer's mental model imo) when you don't have to worry about
+ * adding separate event handlers to every little element. Just a top-level 'mouseover'
+ * handler, for example, which can inspect which element the event originated from trivially
+ * and then decide what to do.
+ * 
+ * I will reimplement GeoJSON functionality with this lightweight ideal in mind in the future.
+ */
+
+// /**
+//  * Used to group several layers and handle them as one. If you add it to the map,
+//  * any layers added or removed from the group will be added/removed on the map as
+//  * well. Extends `Layer`.
+//  *
+//  *  - [`bindTooltip`](#layer-bindtooltip) binds a tooltip to all of the layers at once
+//  *  - Events are propagated to the `LayerGroup`, so if the group has an event
+//  * handler, it will handle events from any of the layers. This includes mouse events
+//  * and custom events.
+//  *
+//  * ```js
+//  * const lg = new LayerGroup([marker1, marker2])
+//  * 	.addLayer(polyline)
+//  * 	.bindTooltip('Hello world!')
+//  * 	.on('click', function() { alert('Clicked on a member of the group!'); });
+//  * 
+//  * map.addLayer(lg);
+//  * ```
+//  */
+// export class LayerGroup extends Layer {
+
+// 	_layers: { [leafletID: string]: Layer } = {};
+
+// 	constructor(layers: Layer[], options?: any) {
+// 		super();
+
+// 		Util.setOptions(this, options);
+
+// 		this._isLayerGroup = true;
+
+// 		for (const layer of layers) {
+// 			this._layers[Util.stamp(layer)] = layer;
+// 		}
+// 	}
+
+// 	onAdd(map: Map): this {
+// 		return this.eachLayer(map.addLayer, map);
+// 	}
+
+// 	onRemove(map: Map): void {
+// 		this.eachLayer(map.removeLayer, map);
+// 	}
+
+// 	/**
+// 	 * Override the default Layer focus functionality to add focus listeners on
+// 	 * every Layer in the group. This was needed for tooltips. -- Sam on 10/18/23
+// 	 */
+// 	_addFocusListeners(): void {
+// 		this.eachLayer(this._addFocusListenersOnLayer, this);
+// 	}
+
+// 	/**
+// 	 * Override the default Layer aria functionality to add descriptions on
+// 	 * every Layer in the group. This was needed for tooltips. -- Sam on 10/18/23
+// 	 */
+// 	_setAriaDescribedBy(): void {
+// 		this.eachLayer(this._setAriaDescribedByOnLayer, this);
+// 	}
+
+// 	/**
+// 	 * Adds the given layer to the group. Does nothing if the layer is already
+// 	 * a member of this group.
+// 	 */
+// 	addLayer(layer: Layer): this {
+// 		const id = Util.stamp(layer);
+
+// 		if (id in this._layers) {
+// 			return this;
+// 		}
+
+// 		layer.addEventParent(this);
+
+// 		this._layers[id] = layer;
+
+// 		if (this._map) {
+// 			this._map.addLayer(layer);
+// 		}
+// 	}
+
+// 	/**
+// 	 * Removes the given layer (or layer ID) from the group. Does nothing if the
+// 	 * given layer is not a member of this group.
+// 	 */
+// 	removeLayer(layerOrID: number | Layer): this {
+// 		const
+// 			id = typeof layerOrID === 'number' ? layerOrID : Util.stamp(layerOrID),
+// 			layer = this._layers[id];
+
+// 		// NOTE: we care that the layer was registered in this LayerGroup's map,
+// 		// not just that we can get ahold of a proper Layer object
+// 		if (!layer) {
+// 			return this;
+// 		}
+
+// 		layer.removeEventParent(this);
+
+// 		if (this._map) {
+// 			this._map.removeLayer(layer);
+// 		}
+
+// 		delete this._layers[id];
+
+// 		return this;
+// 	}
+
+// 	// Returns `true` if the given layer (or layer ID) is currently added to the group.
+// 	hasLayer(layer: number | Layer): boolean {
+// 		const id = typeof layer === 'number' ? layer : Util.stamp(layer);
+// 		return id in this._layers;
+// 	}
+
+// 	// Removes all the layers from the group.
+// 	clearLayers(): this {
+// 		return this.eachLayer(this.removeLayer, this);
+// 	}
+
+// 	/**
+// 	 * Iterates over the layers of the group, optionally specifying context
+// 	 * of the iterator function.
+// 	 *
+// 	 * ```
+// 	 * group.eachLayer(function(layer){
+// 	 *     layer.bindTooltip('Hello');
+// 	 * });
+// 	 * ```
+// 	 */
+// 	eachLayer(method: (l: Layer) => void): this;
+// 	eachLayer<This>(method: (this: This, l: Layer) => void, context: This): this;
+// 	eachLayer(method: (l: Layer) => void, context?: any): this {
+// 		for (const layer of Object.values(this._layers)) {
+// 			method.call(context, layer);
+// 		}
+// 		return this;
+// 	}
+
+// 	// Returns the layer with the given internal ID.
+// 	getLayer(id: number): Layer | undefined {
+// 		return this._layers[id];
+// 	}
+
+// 	// Returns an array of all the layers added to the group.
+// 	getLayers(): Layer[] {
+// 		return Object.values(this._layers);
+// 	}
+
+// 	// Calls `methodName` on every layer contained in this group, passing any
+// 	// additional parameters. Has no effect if the layers contained do not
+// 	// implement `methodName`.
+// 	invoke(methodName: string, ...args: readonly any[]): this {
+// 		for (const layer of Object.values(this._layers)) {
+// 			(layer as any)[methodName]?.apply(layer, args);
+// 		}
+// 		return this;
+// 	}
+
+// 	// Calls `setZIndex` on every layer contained in this group, passing the z-index.
+// 	setZIndex(zIndex: number): this {
+// 		return this.invoke('setZIndex', zIndex);
+// 	}
+
+// 	// Sets the given path options to each layer of the group that has a `setStyle` method.
+// 	setStyle(style: Partial<PathOptions>): this {
+// 		return this.invoke('setStyle', style);
+// 	}
+
+// 	// Brings the layer group to the top of all other layers
+// 	bringToFront(): this {
+// 		return this.invoke('bringToFront');
+// 	}
+
+// 	// Brings the layer group to the back of all other layers
+// 	bringToBack(): this {
+// 		return this.invoke('bringToBack');
+// 	}
+
+// 	// Returns the LatLngBounds of the Feature Group (created from bounds and coordinates of its children).
+// 	getBounds(): LatLngBounds {
+// 		const bounds = new LatLngBounds();
+
+// 		for (
+// 			const layer of Object.values(this._layers)
+// 		) {
+// 			if (layer.getBounds) {
+// 				bounds.extend(layer.getBounds());
+// 			} else if (layer.getLatLng) {
+// 				bounds.extend(layer.getLatLng());
+// 			}
+// 		}
+
+// 		return bounds;
+// 	}
+
+// }
 
 // /**
 //  * Represents a GeoJSON object or an array of GeoJSON objects. Allows you to parse
