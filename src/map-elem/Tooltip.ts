4c357f598aa99e67a834deeae5f412b6bcc8a2ea
Sam Claus <samclaus@protonmail.com>
2023-10-18 15:02:30 -0400

    Merge DivOverlay into Tooltip

--- src/map-elem/Tooltip.ts
+++ src/map-elem/Tooltip.ts
@@ -1,11 +1,27 @@
+import { Layer, type LayerOptions } from '.';
 import { Util, type HandlerMap } from '../core';
 import { DomUtil } from '../dom';
 import { LatLng } from '../geog';
 import { Point } from '../geom';
 import type { Map } from '../map';
-import { DivOverlay, type DivOverlayOptions } from './DivOverlay.js';
 
-export interface TooltipOptions extends DivOverlayOptions {
+export interface TooltipOptions extends LayerOptions {
+	/**
+	 * If true, the tooltip will listen to the mouse events.
+	 */
+	interactive: boolean;
+	/**
+	 * The offset of the overlay position. (0, 0) by default.
+	 */
+	offset: Point;
+	/**
+	 * A custom CSS class name to assign to the overlay.
+	 */
+	className: string | undefined;
+	/**
+	 * Sets the HTML content of the overlay while initializing.
+	 */
+	content: string | HTMLElement;
 	/**
 	 * Direction where to open the tooltip. Possible values are: `right`, `left`,
 	 * `top`, `bottom`, `center`, and `auto`. `auto` (the default) will dynamically
@@ -64,14 +80,39 @@ export interface TooltipOptions extends DivOverlayOptions {
  * - the `tooltipAnchor` Icon option: this will only be considered for Marker. You
  *   should adapt this value if you use a custom icon.
  */
-export class Tooltip extends DivOverlay {
+export class Tooltip extends Layer {
 
 	declare options: TooltipOptions;
+	
+	_latlng: LatLng | undefined;
+	_source: any; // TODO
+	_content: string | HTMLElement;
+	_container = DomUtil.create('div');
+	_contentNode = this._container;
+	_removeTimeout: number | undefined;
 
-	constructor(latlng?: LatLng, options?: Partial<DivOverlayOptions>)
-	constructor(options?: Partial<DivOverlayOptions>, source?: any /* TODO */)
-	constructor(latlngOrOptions?: LatLng | Partial<DivOverlayOptions>, optionsOrSource?: any /* TODO */) {
-		super(latlngOrOptions as any, optionsOrSource);
+	constructor(latlng?: LatLng, options?: Partial<TooltipOptions>)
+	constructor(options?: Partial<TooltipOptions>, source?: any /* TODO */)
+	constructor(latlngOrOptions?: LatLng | Partial<TooltipOptions>, optionsOrSource?: any /* TODO */) {
+		super();
+
+		const optionDefaults = {
+			interactive: false,
+			offset: new Point(0, 0),
+			className: '',
+			pane: undefined,
+			content: ''
+		};
+
+		if (latlngOrOptions instanceof LatLng) {
+			this._latlng = latlngOrOptions;
+			Util.setOptions(this, optionsOrSource, optionDefaults);
+		} else {
+			Util.setOptions(this, latlngOrOptions, optionDefaults);
+			this._source = optionsOrSource;
+		}
+
+		this._content = this.options.content;
 
 		Util.setOptions(
 			this,
@@ -88,7 +129,31 @@ export class Tooltip extends DivOverlay {
 	}
 
 	onAdd(map: Map): this {
-		DivOverlay.prototype.onAdd.call(this, map);
+		this._zoomAnimated = map._zoomAnimated;
+
+		if (!this._container) {
+			this._initLayout();
+		}
+
+		if (map.options.fadeAnimation) {
+			this._container.style.opacity = 0 as any; // will coerce to string
+		}
+
+		clearTimeout(this._removeTimeout);
+		this.getPane()!.appendChild(this._container); // TODO: null safety
+		this.update();
+
+		if (map.options.fadeAnimation) {
+			this._container.style.opacity = 1 as any; // will coerce to string
+		}
+
+		this.bringToFront();
+
+		if (this.options.interactive) {
+			this._container.classList.add('leaflet-interactive');
+			this.addInteractiveTarget(this._container);
+		}
+
 		this.setOpacity(this.options.opacity);
 
 		// @namespace Map
@@ -111,7 +176,17 @@ export class Tooltip extends DivOverlay {
 	}
 
 	onRemove(map: Map): void {
-		DivOverlay.prototype.onRemove.call(this, map);
+		if (map.options.fadeAnimation) {
+			this._container.style.opacity = 0 as any; // will coerce to string
+			this._removeTimeout = setTimeout(() => this._container.remove(), 200);
+		} else {
+			this._container.remove();
+		}
+
+		if (this.options.interactive) {
+			this._container.classList.remove('leaflet-interactive');
+			this.removeInteractiveTarget(this._container);
+		}
 
 		// @namespace Map
 		// @section Tooltip events
@@ -131,8 +206,14 @@ export class Tooltip extends DivOverlay {
 	}
 
 	getEvents(): HandlerMap {
-		const events = DivOverlay.prototype.getEvents.call(this);
+		const events: HandlerMap = {
+			zoom: this._updatePosition,
+			viewreset: this._updatePosition
+		};
 
+		if (this._zoomAnimated) {
+			events.zoomanim = this._animateZoom;
+		}
 		if (!this.options.permanent) {
 			events.preclick = this.close;
 		}
@@ -140,10 +221,162 @@ export class Tooltip extends DivOverlay {
 		return events;
 	}
 
+	// BEGINNING OF STUFF RIPPED FROM DIVOVERLAY
+
+	// Adds the overlay to the map.
+	openOn(map: Map = this._source?._map): this {
+		map.addLayer(this);
+		return this;
+	}
+
+	close(): this {
+		if (this._map) {
+			this._map.removeLayer(this);
+		}
+		return this;
+	}
+
+	// Opens or closes the overlay bound to layer depending on its current state.
+	// Argument may be omitted only for overlay bound to layer.
+	toggle(layer?: Layer): this {
+		if (this._map) {
+			this.close();
+		} else {
+			if (layer) {
+				this._source = layer;
+			} else {
+				layer = this._source;
+			}
+			this._prepareOpen();
+
+			// open the overlay on the map
+			this.openOn(layer!._map); // TODO: null safety
+		}
+		return this;
+	}
+
+	// Returns the geographical point of the overlay.
+	getLatLng(): LatLng {
+		return this._latlng!; // TODO: null safety
+	}
+
+	// Sets the geographical point where the overlay will open.
+	setLatLng(latlng: LatLng): this {
+		this._latlng = latlng;
+
+		if (this._map) {
+			this._updatePosition();
+			this._adjustPan();
+		}
+
+		return this;
+	}
+
+	// Sets the HTML content of the overlay.
+	setContent(content: string | HTMLElement): this {
+		this._content = content;
+		this.update();
+		return this;
+	}
+
+	// Returns the HTML container of the overlay.
+	getElement(): HTMLElement { // TODO: was string | HTMLElement
+		return this._container;
+	}
+
+	// Updates the overlay content, layout and position. Useful for updating the overlay after something inside changed, e.g. image loaded.
+	update(): void {
+		if (!this._map) { return; }
+
+		this._container.style.visibility = 'hidden';
+
+		this._updateContent();
+		this._updateLayout();
+		this._updatePosition();
+
+		this._container.style.visibility = '';
+
+		this._adjustPan();
+	}
+
+	// Returns `true` when the overlay is visible on the map.
+	isOpen(): boolean {
+		return !!(this._map?.hasLayer(this));
+	}
+
+	// Brings this overlay in front of other overlays (in the same map pane).
+	bringToFront(): this {
+		if (this._map) {
+			DomUtil.toFront(this._container);
+		}
+		return this;
+	}
+
+	// Brings this overlay to the back of other overlays (in the same map pane).
+	bringToBack(): this {
+		if (this._map) {
+			DomUtil.toBack(this._container);
+		}
+		return this;
+	}
+
+	// prepare bound overlay to open: update latlng pos / content source (for LayerGroup)
+	_prepareOpen(latlng?: LatLng): boolean {
+		let source = this._source;
+
+		if (!source._map) { return false; }
+
+		// TODO: this code is all coupled, base classes should not know about higher-level classes
+		if (source._isLayerGroup) {
+			// Find the first layer in the feature group that is registered with a map
+			source = Object
+				.values<any>(source._layers)
+				.find(layer => layer._map);
+
+			// None of the feature group layers were registered with a map
+			if (!source) { return false; }
+
+			// set overlay source to this layer
+			this._source = source;
+		}
+
+		latlng ||= (
+			source.getCenter?.() ||
+			source.getLatLng?.() ||
+			source.getBounds?.()?.getCenter()
+		);
+	
+		if (!latlng) {
+			throw new Error('Unable to get source layer LatLng.');
+		}
+
+		this.setLatLng(latlng);
+		this.update(); // update the overlay (content, layout, etc...)
+
+		return true;
+	}
+
+	_updateContent(): void {
+		const
+			node = this._contentNode,
+			content = this._content;
+
+		if (typeof content === 'string') {
+			node.innerHTML = content;
+		} else {
+			DomUtil.removeAllChildren(node);
+			node.appendChild(content);
+		}
+
+		this.fire('contentupdate');
+	}
+
+	// END OF STUFF RIPPED FROM DIVOVERLAY
+
 	_initLayout(): void {
 		const
 			prefix = 'leaflet-tooltip',
-		    className = `${prefix} ${(this.options as any /* TODO */).className || ''} leaflet-zoom-${this._zoomAnimated ? 'animated' : 'hide'}`;
+		    className = `${prefix} ${this.options.className || ''} leaflet-zoom-${this._zoomAnimated ? 'animated' : 'hide'}`;
 
 		this._contentNode = this._container = DomUtil.create('div', className);
 		this._container.setAttribute('role', 'tooltip');
--- src/map-elem/index.ts
+++ src/map-elem/index.ts
@@ -1,9 +1,7 @@
 export * from './BlanketOverlay.js';
-export * from './DivOverlay.js';
 // export { GeoJSON, asFeature, coordsToLatLng, coordsToLatLngs, geometryToLayer, getFeature, latLngToCoords, latLngsToCoords } from './GeoJSON.js';
 export * from './ImageOverlay.js';
 export * from './Layer.js';
-export * from './LayerGroup.js';
 export * from './SVGOverlay.js';
 export * from './Tooltip.js';
 export * from './VideoOverlay.js';
